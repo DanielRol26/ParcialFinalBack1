c435c68441263436a99d3a40e2e9da980eb79061
AndresAcosta01 <andresacost12a@gmail.com>
2026-05-26 14:16:49 -0500

    feat: creacion de backend

---
+++ backend/InfluConnect/src/main/java/com/example/InfluConnect/InfluConnectApplication.java
@@ -0,0 +1,13 @@
+package com.example.InfluConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfluConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfluConnectApplication.class, args);
+	}
+
+}
---
+++ backend/InfluConnect/src/test/java/com/example/InfluConnect/InfluConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.InfluConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfluConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
